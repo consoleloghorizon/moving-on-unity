7f4d293b00e1474bd543e42497945cba2c38beb7
Greg <greg@Gregs-MacBook-Pro.local>
2019-01-26 18:13:28 -0500

    Added Damaging Collisions

---
+++ Assets/Scripts/World/ContactHazard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactHazard : MonoBehaviour
+{
+
+    public float damageOnHit;
+
+    private Player player;
+
+    void Start()
+    {
+        player = Player.instance;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Collison");
+        if (col.gameObject.CompareTag("Player"))
+        {
+            player = col.gameObject.GetComponentInParent<Player>();
+
+            if (player != null)
+            {
+                player.ApplyDamage(damageOnHit);
+            }
+        }
+    }
+}
