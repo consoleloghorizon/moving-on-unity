0408ae275bdfb03b7519ffdc607cda3ebafeb064
Daniel Streicher <dstreicher@bitbucket.org>
2019-01-26 15:32:13 -0500

    updated pivot points in background sprites and platformTemplate scene

---
+++ Assets/Scripts/Camera/CameraZoom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public Camera mainCamera;
+    public float dampTime = 0.15f;
+    public float sizeValue = 4.0f;
+
+    void Start() {
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        Debug.Log("OnCollisionEnter: " + col.gameObject.name);
+        //mainCamera.orthographicSize = sizeValue;
+        float initialSize = mainCamera.orthographicSize;
+        mainCamera.orthographicSize = Mathf.Lerp(initialSize, sizeValue, dampTime * Time.deltaTime);
+    }
+}
