069af04dfef80d9e8a55e545d0775f6fdc8044fb
Daniel Streicher <dstreicher@bitbucket.org>
2019-01-27 12:58:57 -0500

    added music to each level

---
+++ Assets/Scripts/World/PlayMusic.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour
+{
+    public AudioClip sceneMusic;
+
+    private void Start() {
+        SoundMan.Instance.PlayMusic(sceneMusic);
+    }
+}
